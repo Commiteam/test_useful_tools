bed1b60e47fc343f588a63de004476d675c48a8c
bagashvilit <bagashvilit@allegheny.edu>
2021-06-27 22:30:39 -0400

    chore: Update rev id

--- src/collect.py
+++ src/collect.py
@@ -1,3 +1,5 @@
-from reporover.collect import collect
+# from reporover.collect import collect
+import model
+# collect("https://github.com/GatorEducator/gatorgrader")
 
-collect("https://github.com/GatorEducator/gatorgrader")
+print(model.load())
