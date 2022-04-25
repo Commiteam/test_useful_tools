9a9dc997a466d65978454c0adeef2e398472b79b
bagashvilit <bagashvilit@allegheny.edu>
2022-04-25 16:37:51 -0400

    Update rev id

---
+++ commitcanvas_plugins.py
@@ -0,0 +1,10 @@
+import commitcanvas
+
+
+class min_length:
+    @commitcanvas.hookimpl
+    def rule(self,message):
+        min_count = 2
+        count = len(message.split(" "))
+        if count <= min_count:
+            return("Commit must have more than 2 words, got: {}".format(count)) 
