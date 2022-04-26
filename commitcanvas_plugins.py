1f409f99d09810a3a8e9f2dade3726e1d790066c
bagashvilit <bagashvilit@allegheny.edu>
2022-04-26 15:18:50 -0400

    Update rev id

--- commitcanvas_plugins.py
+++ commitcanvas_plugins.py
@@ -9,9 +9,3 @@ class subject_min_word_count:
         if count <= min_count:
             return("Commit message must have more than {} words, got: {}".format(min_count,count))
 
-# class subject_capital_letter:
-#     @commitcanvas.check
-#     def rule(self,message:str):
-#         """Subject line of commit message starts with capital letter."""
-#         if message[0].isupper():
-#             return("Subject must start with lower case letter") 
