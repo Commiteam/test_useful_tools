8faea867371c702fdc8e4661356d7df81597ff06
bagashvilit <bagashvilit@allegheny.edu>
2022-04-25 20:11:36 -0400

    update.This is body

--- commitcanvas_plugins.py
+++ commitcanvas_plugins.py
@@ -1,15 +1,10 @@
 import commitcanvas
 
 
-class min_length:
-    @commitcanvas.hookimpl
+class subject_min_word_count:
+    @commitcanvas.check
     def rule(self,message):
         min_count = 2
         count = len(message.split(" "))
         if count <= min_count:
-            return("Commit must have more than 2 words, got: {}".format(count)) 
-
-class plug1:
-    @commitcanvas.hookimpl
-    def rule(self,message:str):
-        pass
+            return("Commit must have more than {} words, got: {}".format(min_count,count)) 
