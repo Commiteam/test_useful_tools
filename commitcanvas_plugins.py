d09169f7a269f6e595abd4b180f80c68b3059a09
bagashvilit <bagashvilit@allegheny.edu>
2022-06-10 05:35:41 -0400

    Update rev id

--- commitcanvas_plugins.py
+++ commitcanvas_plugins.py
@@ -1,6 +1,13 @@
 import commitcanvas
+import textblob
+# import pandas as pd
 
+# s = pd.Series([1, 3, 5, 6, 8])
+# print(s)
 
+def helper(input):
+    return(input)
+    
 class subject_min_word_count:
     @commitcanvas.check
     def rule(self,message):
@@ -8,4 +15,12 @@ class subject_min_word_count:
         count = len(message.split(" "))
         if count <= min_count:
             return("Commit message must have more than {} words, got: {}".format(min_count,count))
+
+
+class subject_start_verb:
+    @commitcanvas.check
+    def rule(self,message):
+        blob = textblob.TextBlob(message)
+        if blob.tags[0][1] not in "VB, NNP":
+            return("Commit message must start with word in imperative")
 
